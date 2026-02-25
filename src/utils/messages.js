71decee6d185082f301fe0bf8a547c519d44860f
Rakesh Dey <rakeshdey2027@gmail.com>
2026-02-25 10:23:20 +0600

    init commit

---
+++ src/utils/messages.js
@@ -0,0 +1,23 @@
+const generateMessage = (username, text) => {
+   return {
+    username,
+    text,
+    createdAt: Date.now()
+
+   }
+};
+
+const generateLocationMessage = (username, url) => {
+    return {
+        username,
+        url,
+        createdAt: Date.now()
+
+    }
+};
+
+module.exports = {
+    generateMessage, 
+    generateLocationMessage
+}
+
